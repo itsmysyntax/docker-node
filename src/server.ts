3069bde87f4201a5556a3d3ba8a138bc32bbb601
Your Name <you@example.com>
2020-08-11 04:41:44 +0000

    all

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ if (process.env.MONGO_URL == null) {
   mongoConnection.connect(() => {
     app.listen(app.get('port'), (): void => {
       console.log('\x1b[36m%s\x1b[0m', // eslint-disable-line
-        `🌏 Express server started at http://localhost:${app.get('port')}`);
+        `🌏 Express server startedat http://localhost:${app.get('port')}`);
       if (process.env.NODE_ENV === 'development') {
         console.log('\x1b[36m%s\x1b[0m', // eslint-disable-line
           `⚙️  Swagger UI hosted at http://localhost:${app.get('port')}/dev/api-docs`);
